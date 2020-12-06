618a0e0e1af80afa371ba332e9a86ddabdbb3a7a
David Humphrey <david.andrew.humphrey@gmail.com>
2020-12-05 22:37:58 -0500

    Support single observations on map

--- components/Map/MultipleLocationsMap.js
+++ components/Map/MultipleLocationsMap.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import { Markers } from './Markers';
 
 // Fill the available space of the parent. Set the desired dimensions on parent.
@@ -8,9 +8,26 @@ const style = {
   width: '100%',
 };
 
+function ChangeView({ center, zoom }) {
+  const map = useMap();
+  if (center) {
+    map.setView(center, zoom);
+  }
+  return null;
+}
+
 export default function MultipleLocationsMap({ observations, bounds }) {
+  const zoom = 11;
+
+  let center;
+  if (observations.length === 1) {
+    const { lat, lng } = observations[0];
+    center = [lat, lng];
+  }
+
   return (
-    <MapContainer style={style} bounds={bounds} scrollWheelZoom={false} zoom={11}>
+    <MapContainer style={style} center={center} bounds={bounds} scrollWheelZoom={false} zoom={11}>
+      <ChangeView center={center} zoom={zoom} />
       <TileLayer
         url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
