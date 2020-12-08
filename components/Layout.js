403815d347ee4162261805c358b4150522789fd5
David Humphrey <david.andrew.humphrey@gmail.com>
2020-12-07 19:27:17 -0500

    Better seo

--- components/Layout.js
+++ components/Layout.js
@@ -7,19 +7,24 @@ import styles from './Layout.module.css';
 export const siteTitle = 'SnowyOwls.ca';
 
 export default function Layout({ children, title, includeRegion }) {
+  const description = 'Tracking Snow Owls Across Canada';
+
   return (
     <section className={styles.layout}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Tracking Snow Owls Across Canada" />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
+        <meta name="description" content={description} />
+
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="/owls/on-the-fence/medium.jpg" />
+        <meta property="og:url" content="https://snowyowls.ca" />
+
+        <meta name="twitter:title" content={siteTitle} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="/owls/on-the-fence/medium.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image:alt" content="Snowy Owl on fence post" />
       </Head>
 
       <Navigation
