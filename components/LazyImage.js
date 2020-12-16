b68d79f667fe0192999180efaf3b3dfac5e9cbca
David Humphrey <david.andrew.humphrey@gmail.com>
2020-12-15 22:21:46 -0500

    Include LazyImage component

---
+++ components/LazyImage.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import Figure from 'react-bootstrap/Figure';
+import Image from 'react-bootstrap/Image';
+
+export default function LazyImage({ imgDir, caption, author, authorUrl }) {
+  return (
+    <Figure style={{ width: '100%' }}>
+      <Image
+        src={`${imgDir}/800.jpg`}
+        srcSet={`${imgDir}/400.jpg 400w, ${imgDir}/800.jpg 800w`}
+        loading="lazy"
+        fluid
+      />
+      <Figure.Caption className="text-center">
+        {caption}. Photo by <a href={authorUrl}>{author}</a>
+      </Figure.Caption>
+    </Figure>
+  );
+}
+
+LazyImage.propTypes = {
+  imgDir: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  authorUrl: PropTypes.string.isRequired,
+};
