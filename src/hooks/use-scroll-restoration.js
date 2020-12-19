3f0296a5a9ed39bf07d7a7c87e61de49721ea3bc
David Humphrey <david.andrew.humphrey@gmail.com>
2020-12-18 21:11:47 -0500

    Add offset adjustment to scroll restoration for images that haven't loaded yet

--- src/hooks/use-scroll-restoration.js
+++ src/hooks/use-scroll-restoration.js
@@ -1,25 +1,53 @@
 // Next.js doesn't seem to know how to do scroll restoration properly.
-// This isn't perfect, but it's something.
-// Modified version of https://github.com/vercel/next.js/issues/3303#issuecomment-628400930
+// This isn't perfect, but it's something. Modified version of
+// https://github.com/vercel/next.js/issues/3303#issuecomment-628400930
 import { useEffect } from 'react';
 
 import Router from 'next/router';
 
-function getScrollTop() {
-  return document.documentElement.scrollTop;
+function getScrollInfo() {
+  const docElem = document.documentElement;
+  return {
+    // How far down we are currently scrolled
+    scrollTop: docElem.scrollTop,
+    // The total scroll height
+    scrollHeight: docElem.scrollHeight,
+    // The viewport height
+    innerHeight: window.innerHeight,
+  };
 }
 
-function saveScrollTop(url) {
-  const scrollPos = { top: getScrollTop() };
-  sessionStorage.setItem(url, JSON.stringify(scrollPos));
+// HACK: adjust scrollTop to deal with images that haven't finished loading yet.
+// We know what the scrollHeight *will* be (or, was when we recorded it), and
+// since we know that things are basically the same (window.innerHeight is same),
+// we can adjust now for the difference, instead of scrolling too far.
+function adjustHeight(top, height) {
+  const { scrollHeight } = getScrollInfo();
+  const diff = height - scrollHeight;
+  if (diff) {
+    return top - diff;
+  }
+  return top;
 }
 
-function restoreScrollTop(url) {
+function saveScroll(url) {
+  const scrollInfo = getScrollInfo();
+  sessionStorage.setItem(url, JSON.stringify(scrollInfo));
+}
+
+function restoreScroll(url) {
   try {
-    const scrollPos = JSON.parse(sessionStorage.getItem(url));
-    if (scrollPos) {
-      window.requestAnimationFrame(() => window.scrollTo(0, scrollPos.top));
+    const scrollInfo = JSON.parse(sessionStorage.getItem(url));
+    if (!scrollInfo) {
+      return;
     }
+    // Bail if the window has changed size since we recorded the scroll position
+    if (window.innerHeight !== scrollInfo.innerHeight) {
+      return;
+    }
+
+    const top = adjustHeight(scrollInfo.scrollTop, scrollInfo.scrollHeight);
+    window.requestAnimationFrame(() => window.scrollTo(0, top));
   } catch (err) {
     // We tried, let the scrollbar fall where it may!
   }
@@ -33,21 +61,21 @@ export default function useScrollRestoration(router) {
 
     let shouldScrollRestore = false;
     window.history.scrollRestoration = 'manual';
-    restoreScrollTop(router.pathname);
+    restoreScroll(router.pathname);
 
     const onBeforeUnload = (event) => {
-      saveScrollTop(router.pathname);
+      saveScroll(router.pathname);
       delete event['returnValue'];
     };
 
     const onRouteChangeStart = () => {
-      saveScrollTop(router.pathname);
+      saveScroll(router.pathname);
     };
 
     const onRouteChangeComplete = (url) => {
       if (shouldScrollRestore) {
         shouldScrollRestore = false;
-        restoreScrollTop(url);
+        restoreScroll(url);
       }
     };
 
